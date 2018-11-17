b23f3ec32962c8672a0aadb3eb44ba288f379260
Tino Krüger <krueger.tino@googlemail.com>
2018-11-17 20:23:00 +0100

    Added an extension for IEnumerable for asynchronous processing

---
+++ Eshava.Core.Linq/Extensions/IEnumerableExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Eshava.Core.Linq.Extensions
+{
+	public static class IEnumerableExtension
+	{
+		public static async Task<IEnumerable<T>> WhereAsync<T>(this IEnumerable<T> items, Func<T, Task<bool>> expression)
+		{
+			var results = new List<T>();
+
+			foreach (var item in items)
+			{
+				if (await expression(item))
+				{
+					results.Add(item);
+				}
+			}
+
+			return results;
+		}
+
+		public static async Task<bool> AnyAsync<T>(this IEnumerable<T> items, Func<T, Task<bool>> expression)
+		{
+			foreach (var item in items)
+			{
+				if (await expression(item))
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
