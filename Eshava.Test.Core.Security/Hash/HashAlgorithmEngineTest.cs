e8a781089aec5c963f7d8eaeb51c6e5f2a8479fc
Tino Krüger <krueger.tino@googlemail.com>
2019-10-28 17:29:49 +0100

    [Security] implemented compute and verfiy hash functionality

---
+++ Eshava.Test.Core.Security/Hash/HashAlgorithmEngineTest.cs
@@ -0,0 +1,67 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using Eshava.Core.Security.Hash;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Eshava.Test.Core.Security.Hash
+{
+	[TestClass, TestCategory("Core.Security")]
+	public class HashAlgorithmEngineTest
+	{
+		private HashAlgorithmEngine _classUnderTest;
+
+		[TestInitialize]
+		public void Setup()
+		{
+			_classUnderTest = new HashAlgorithmEngine(new SHA256Managed());
+		}
+
+		[TestMethod]
+		public void ComputeAndVerifyTest()
+		{
+			// Arrange
+			var hashOne = Encoding.UTF8.GetBytes("Darkwing Duck");
+			var hashTwo = Encoding.UTF8.GetBytes("MegaVolt");
+
+			// Act
+			var resultHash = _classUnderTest.Compute(hashOne, hashTwo);
+			var resultVerify = _classUnderTest.Verify(hashOne, hashTwo, resultHash);
+
+			// Assert
+			resultVerify.Should().BeTrue();
+		}
+
+		[TestMethod]
+		public void VerifyHashDifferentLenghtTest()
+		{
+			// Arrange
+			var hashOne = Encoding.UTF8.GetBytes("Darkwing Duck");
+			var hashTwo = Encoding.UTF8.GetBytes("MegaVolt");
+			var resultHash = Encoding.UTF8.GetBytes("LaunchpadMcQuack");
+
+			// Act
+			var resultVerify = _classUnderTest.Verify(hashOne, hashTwo, resultHash);
+
+			// Assert
+			resultVerify.Should().BeFalse();
+		}
+
+		[TestMethod]
+		public void VerifyHashNotEqualTest()
+		{
+			// Arrange
+			var hashOne = Encoding.UTF8.GetBytes("Darkwing Duck");
+			var hashTwo = Encoding.UTF8.GetBytes("MegaVolt");
+
+			// Act
+			var resultHash = _classUnderTest.Compute(hashOne, hashTwo);
+			resultHash[0] = 255;
+
+			var resultVerify = _classUnderTest.Verify(hashOne, hashTwo, resultHash);
+
+			// Assert
+			resultVerify.Should().BeFalse();
+		}
+	}
+}
