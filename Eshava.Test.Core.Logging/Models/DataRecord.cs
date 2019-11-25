faf5b63aecc03a104ddc608a52a24f76465df127
Tino Krüger <krueger.tino@googlemail.com>
2019-11-25 19:59:38 +0100

    [Logging] used underlying data types for change tracker

--- Eshava.Test.Core.Logging/Models/DataRecord.cs
+++ Eshava.Test.Core.Logging/Models/DataRecord.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Eshava.Test.Core.Logging.Models
@@ -17,5 +18,8 @@ namespace Eshava.Test.Core.Logging.Models
 		public string RecordValue { get; set; }
 
 		public string RecordVersion { get; set; }
+
+		[Column("TimestampColumn")]
+		public DateTime? Timestamp { get; set; }
 	}
 }
