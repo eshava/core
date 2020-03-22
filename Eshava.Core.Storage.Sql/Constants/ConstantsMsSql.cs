c67ce8f84312a00bf5dfe005e36836b96e95ddbe
Tino Krüger <krueger.tino@googlemail.com>
2020-03-22 13:54:41 +0100

    [Storage.Sql] Implemented storage package for sql database administration - added MsSql database engine

---
+++ Eshava.Core.Storage.Sql/Constants/ConstantsMsSql.cs
@@ -0,0 +1,75 @@
+﻿namespace Eshava.Core.Storage.Sql.Constants
+{
+	internal static class ConstantsMsSql
+	{
+		public const string COMMENT = "--";
+		public const string STRING = "'";
+		public const string ON = "ON";
+		public const string OFF = "OFF";
+		public const string CREATE = "CREATE";
+		public const string USE = "USE";
+		public const string DROP = "DROP";
+		public const string BACKUP = "BACKUP";
+		public const string RESTORE = "RESTORE";
+		public const string WITH = "WITH";
+		public const string EXEC = "EXEC";
+		public const string DATABASE = "DATABASE";
+		public const string USER = "USER";
+
+		public const string IF = "IF";
+		public const string ISNOTNULL = "IS NOT NULL";
+		public const string BEGIN = "BEGIN";
+		public const string END = "END";
+		public const string DECLARE = "DECLARE";
+
+		public const string SELECT = "SELECT";
+		public const string FROM = "FROM";
+		public const string WHERE = "WHERE";
+		public const string LEFTOUTERJOIN = "LEFT OUTER JOIN";
+		public const string GROUPBY = "GROUP BY";
+		public const string DISK = "DISK";
+		public const string MOVE = "MOVE";
+		public const string TO = "TO";
+		public const string STATS = "STATS";
+		public const string CONVERT = "CONVERT";
+		public const string AS = "AS";
+
+		public const string MAX = "MAX";
+
+
+		public const string MASTER = "master";
+		public const string MODEL = "model";
+		public const string MSDB = "msdb";
+		public const string TEMPDB = "tempdb";
+		public const string DBO = "dbo";
+
+		public const string LOGIN = "LOGIN";
+		public const string PASSWORD = "PASSWORD";
+		public const string CHECK_POLICY = "CHECK_POLICY";
+		public const string DEFAULT_DATABASE = "DEFAULT_DATABASE";
+		public const string GRANT_EXECUTE_TO = "GRANT EXECUTE TO";
+
+		public const string SP_CHANGEDBOWNER = "sp_changedbowner";
+		public const string SP_ADDROLEMEMBER = "sp_addrolemember";
+
+		public const string DBROLE_READER = "db_datareader";
+		public const string DBROLE_WRITER = "db_datawriter";
+		public const string DBROLE_BACKUP = "db_backupoperator";
+		public const string DBROLE_DDLADMIN = "db_ddladmin";
+
+		public const string FILE_EXTENSION_BAK = ".bak";
+		public const string FILE_EXTENSION_MDF = ".mdf";
+		public const string FILE_EXTENSION_LDF = ".ldf";
+
+		public const string LOG = "LOG";
+		public const string TODISK = "TO DISK";
+		public const string WITHFORMAT = "WITH FORMAT";
+		public const string MEDIANAME = "MEDIANAME";
+		public const string NAME = "NAME";
+		public const string FILENAME = "FILENAME";
+		public const string SIZE = "SIZE";
+		public const string MAXSIZE = "MAXSIZE";
+		public const string FILEGROWTH = "FILEGROWTH";
+		public const string MB = "MB";
+	}
+}
