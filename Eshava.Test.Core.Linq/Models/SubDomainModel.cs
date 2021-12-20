721d3d9111d33ae245d7284bdf32756ed103ad04
TinoKrueger <krueger.tino@googlemail.com>
2021-12-20 13:45:47 +0100

    [Linq] Added tests for TransformQueryEngine

---
+++ Eshava.Test.Core.Linq/Models/SubDomainModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Eshava.Test.Core.Linq.Models
+{
+	public class SubDomainModel
+	{
+		public int SubId { get; set; }
+		public string SubName { get; set; }
+		public int? NullableStuff { get; set; }
+		public SubSubDomainModel Sub { get; set; }
+	}
+}
---
+++ Eshava.Test.Core.Linq/Models/SubSubDataModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Eshava.Test.Core.Linq.Models
+{
+	public class SubSubDataModel
+	{
+		public int? SubId { get; set; }
+		public string SubName { get; set; }
+	}
+}
