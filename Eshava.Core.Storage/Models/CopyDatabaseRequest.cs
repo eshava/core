6db97175b250f5c6e78baf01c3d72c2574e221cc
TinoKrueger <krueger.tino@googlemail.com>
2023-11-17 21:24:06 +0100

    [Storage] Added copy database request model

---
+++ Eshava.Core.Storage/Models/CopyDatabaseRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Eshava.Core.Storage.Models
+{
+	public class CopyDatabaseRequest
+	{
+		public DatabaseConnectionOptions Server { get; set; }
+		public string DatabaseNameSource { get; set; }
+		public string DatabaseNameTarget { get; set; }
+		public string BackupPath { get; set; }
+		public string TargetFilePathData { get; set; }
+		public string TargetFilePathLog { get; set; }
+	}
+}
